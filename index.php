52b1727a679c4b313fd5c988f7df9c959c6b6573
golf <golf_cat1@hotmail.com>
2017-05-11 23:36:34 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="./css/index.css">
+    </head>
+    <body>
+        <div class="bar">
+            
+              <div class="inside">  <a href="%"> Login</a> &nbsp;|&nbsp; <a href="%"> Register </a> </div>
+            
+        </div>
+    </body>
+</html>
---
+++ register.php
@@ -0,0 +1,18 @@
+<html>
+
+    <head>
+        
+    </head>
+    <body>
+        <form action="checkRegister.php" method="post">
+            First Name: <input type="" name="" value="">
+            Last Name: <input type="" name="" value="">
+            Email: <input type="" name="" value="">
+            Username: <input type="" name="" value="">
+            Password: <input type="" name="" value="">
+            Confirm Password: <input type="" name="" value="">
+
+            <input type="submit" name="" value="">
+        </form>
+    </body>
+</html>
